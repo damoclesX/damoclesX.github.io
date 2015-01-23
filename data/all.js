3aff3db945f324dbec6ddab76967e89a3cfdd6c1
damoclesX <404165481@qq.com>
2015-01-23 17:03:10 +0800

    修改菜单链接，新增一条数据

--- data/all.js
+++ data/all.js
@@ -4,5 +4,11 @@
         desc:'',
         pubdate:'2015-1-23 16:39:43',
         url:'1.html'
+    },
+    {
+        name:'welcome to github pages2',
+        desc:'',
+        pubdate:'2015-1-23 17:02:06',
+        url:'2.html'
     }
 ]
