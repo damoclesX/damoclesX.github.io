c09c057ee5a11391a18a2dd06126c483ce0ca251
damoclesX <404165481@qq.com>
2015-01-23 17:05:44 +0800

    修复增加数据的bug

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ ready(function(){
         var data = eval(data);
         var str ='';
         for(var i=0;i<data.length;i++){
-            str = '<li><h3><a href="'+data[i].url+'">'+data[i].name+'</a></h3><small>'+data[i].pubdate+'</small><p>'+data[i].desc+'</p></li>'
+            str += '<li><h3><a href="'+data[i].url+'">'+data[i].name+'</a></h3><small>'+data[i].pubdate+'</small><p>'+data[i].desc+'</p></li>'
         }
         list.innerHTML = str;
         loading.style.display = 'none';
